f0969b2d98363f16a78c9af88c4b32a1bd8aae0f
Idrees Haddad <gelnox@live.com>
2022-05-17 19:17:26 +0300

    Added students/list

--- MB.MCPA2022.Web/Controllers/HomeController.cs
+++ MB.MCPA2022.Web/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace MB.MCPA2022.Web.Controllers
             return View();
         }
 
+        public IActionResult Employee()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ MB.MCPA2022.Web/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MB.MCPA2022.Web.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
