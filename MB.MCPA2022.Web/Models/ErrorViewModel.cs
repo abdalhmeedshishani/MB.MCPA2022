05746ce67ee09efe6bf152d06acae53cf6a9bb19
Idrees Haddad <gelnox@live.com>
2022-05-17 17:52:50 +0300

    Add project files.

---
+++ MB.MCPA2022.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MB.MCPA2022.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
